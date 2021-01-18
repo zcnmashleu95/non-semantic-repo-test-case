018551c6dfeed2eba0524844e3d03f2d63ac464d
Gimpaya <yralle.lesly.gimpaya@accenture.com>
2021-01-18 16:35:28 +0800

    Second Commit

--- repo-test/repo-test/repo-test.cpp
+++ repo-test/repo-test/repo-test.cpp
@@ -6,8 +6,18 @@
 
 using namespace std;
 
+// This is a function to Square an Integer
+
+int square(int x) {
+    return x * x;
+}
 int main()
 {
     cout << "Hello World!\n";
+
+    int x = square(5);
+    cout << "I am squaring 5\n";
+    cout << x;
+
 }
 
